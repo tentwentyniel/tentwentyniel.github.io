fd60499cf1a55f97036d175bc610b63310baff2d
niel <niel@tentwenty.me>
2018-08-30 17:16:02 +0400

    updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./app.css";
-import App from "./pages/App";
-import registerServiceWorker from "./registerServiceWorker";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-registerServiceWorker();
